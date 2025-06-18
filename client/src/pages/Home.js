1982673a6bdf6b70092bc0b1d3b53680c34d2bc9
NandiniNarayanaswamy <nandininarayanasamy1997@gmail.com>
2025-06-18 16:51:45 +0530

    changed done

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -86,7 +86,7 @@ const Home = () => {
                                 />
                                 <h3>{trainer.name}</h3>
                                 <p>{trainer.specialization}</p>
-                                <button onClick={() => navigate(`/user/trainer/${trainer._id}`)}>Learn More</button>
+                                <button onClick={() => navigate(`/user/trainers/${trainer._id}`)}>Learn More</button>
                             </div>
                         ))}
                     </div>
